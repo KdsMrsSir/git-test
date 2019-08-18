dd57ab5af5e718f47b598b6f1b6db2a1d0c8f332
zhangsan <971735930@qq.com>
2019-08-18 22:49:57 +0800

    新增GitFirst

---
+++ Git-Test/src/com/itheima/git/GitFirst.java
@@ -0,0 +1,19 @@
+package com.itheima.git;
+
+/**
+ * @program: Git-Test
+ * @类描述: ${description}
+ * @日期: 2019-08-18 22:48
+ */
+
+public class GitFirst {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
